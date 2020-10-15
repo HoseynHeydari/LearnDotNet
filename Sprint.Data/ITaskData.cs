bcf2715552055904e24dda98b2b926c6ff9e47cc
Hoseyn <hhma1371@gmail.com>
2020-10-15 18:04:50 +0330

    Add in memory tasks data. Add singleton service for data.

--- Sprint.Data/ITaskData.cs
+++ Sprint.Data/ITaskData.cs
@@ -1,6 +1,11 @@
-﻿namespace Sprint.Data
+﻿using System.Collections.Generic;
+using Sprint.Core;
+
+namespace Sprint.Data
 {
     public interface ITaskData
     {
+        Task GetById(int id);
+        public IEnumerable<Task> GetAllTasks();
     }
 }
--- Sprint.Data/InMemoryTaskData.cs
+++ Sprint.Data/InMemoryTaskData.cs
@@ -1,7 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+using Sprint.Core;
+
 namespace Sprint.Data
 {
     public class InMemoryTaskData : ITaskData
     {
-        
+        List<Task> tasks;
+        public InMemoryTaskData()
+        {
+            tasks = new List<Task>
+            {
+                new Task { Id = 1, Name = "Clean Code", Percent = Percentage.ZERO },
+                new Task { Id = 2, Name = "Dirty Code", Percent = Percentage.ZERO },
+                new Task { Id = 3, Name = "Continus Integration", Percent = Percentage.ZERO },
+                new Task { Id = 4, Name = "Code Review", Percent = Percentage.ZERO },
+                new Task { Id = 5, Name = "Add Feature Hi", Percent = Percentage.ZERO },
+            };
+        }
+        public Task GetById(int id)
+        {
+            return tasks.SingleOrDefault(t => t.Id == id);
+        }
+
+        public IEnumerable<Task> GetAllTasks()
+        {
+            return from task in tasks
+                   orderby task.Name
+                   select task;
+        }
     }
 }
